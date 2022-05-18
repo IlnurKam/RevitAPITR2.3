92616bcdf1ee7cf5546d7257df1e6cbb8b617e4a
IlnurKam <kamal673@yandex.ru>
2022-05-18 22:12:57 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITR2.3/ColumnsFilter.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITR2._3
+{
+    internal class ColumnsFilter : ISelectionFilter
+    {
+
+        public bool AllowElement(Element elem)
+        {
+            return elem is Сolumn;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
---
+++ RevitAPITR2.3/Main.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITR2._3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new ColumnFilter(), "Выберите элементы");
+            var columnList = new List<Wall>();
+
+            string info = string.Empty;
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Column oColumn = doc.GetElement(selectedElement) as Wall;
+                columnList.Add(oColumn);
+                var width = UnitUtils.ConvertFromInternalUnits(oColumn.Width, UnitTypeId.Millimeters);
+                info += $"Name:{oColumn.Name}, width: {width}{Environment.NewLine}";
+
+            }
+            info += $"Количество: {columnList.Count}";
+
+            TaskDialog.Show("Selection", info);
+
+            return Result.Succeeded;
+        }
+    }
+}
